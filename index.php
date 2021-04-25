02fc433dc2e59a7a21c38769b78dc5eff9215137
iamjada <jsenebouttarath@mail.greenriver.edu>
2021-04-24 23:44:53 -0400

    added index.php

---
+++ index.php
@@ -0,0 +1,26 @@
+<?php
+
+// https://github.com/iamjada/dating
+// This is my controller for the dating project
+
+// Turn on error reporting
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
+// Require autoload file
+require_once('vendor/autoload.php');
+
+// instantiate fat-free
+$f3 = Base::instance();
+
+// define routes
+$f3->route('GET /', function(){
+
+    // Display the home page
+    $view = new Template(); // instantiate view object
+    echo $view->render('views/home.html'); // using view object to display the view page
+
+});
+
+// run fat-free
+$f3->run();
