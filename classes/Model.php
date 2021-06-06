cf32b9ac19a1824e56bb4a67e3299675e1fa41b9
iamjada <jsenebouttarath@mail.greenriver.edu>
2021-06-06 07:01:32 -0400

    updated for dating iv

---
+++ classes/Model.php
@@ -0,0 +1,8 @@
+<!-- 328/dating/classes/Controller.php-->
+<?php
+class Model{
+    // this class will handle validation
+
+
+
+}
--- index.php
+++ index.php
@@ -11,226 +11,45 @@ error_reporting(E_ALL);
 
 // Require autoload file
 require_once('vendor/autoload.php');
-require_once('model/validation.php');
-require_once('model/data-layer.php');
+//require_once('model/Validation.php');
+//require_once('model/DataLayer.php');
 
 //Start a session
 session_start();
 
 // instantiate fat-free
 $f3 = Base::instance();
-//$con = new Controller($f3);
+$con = new Controller($f3);
+//$dataLayer = new DataLayer();
 
-/*
-//Define default route
-$f3->route('GET /', function(){
+//Define default route (home page)
+$f3->route('GET|POST /', function(){
 
     $GLOBALS['con']->home();
 });
-*/
 
-// home page
-$f3->route('GET /', function(){
+// profile1
+$f3->route('GET|POST /profile1', function(){
 
-    // Display the home page
-    $view = new Template(); // instantiate view object
-    echo $view->render('views/home.html'); // using view object to display the view page
-
+    $GLOBALS['con']->profile1();
 });
 
-//personal-info.html
-$f3->route('GET|POST /profile1', function ($f3) {
+//personal-info
+$f3->route('GET|POST /profile2', function(){
 
-    //Reinitialize a session array
-    $_SESSION = array();
-
-    //Initialize variables to store user input
-    $userFName = "";
-    $userLName ="";
-    $userAge = "";
-    $userPhone = "";
-    $userGender ="";
-
-    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-        //var_dump($_POST);
-        // validate first name
-        if (validFName($_POST['fName'])) {
-            $_SESSION['fName'] = $userFName;
-        } //Otherwise, set an error variable in the hive
-        else {
-            $f3->set('errors["fName"]', 'Please enter a first name');
-        }
-        // validate last name
-        if (validLName($_POST['lName'])) {
-            $_SESSION['lName'] = $_POST['lName'];
-        } //Otherwise, set an error variable in the hive
-        else {
-            $f3->set('errors["lName"]', 'Please enter a last name');
-        }
-
-        // validate age and store data
-       if (validAge($_POST['age'])) {
-           $_SESSION['age'] = $_POST['age'];
-       }
-       else {
-           $f3->set('errors["age"]', 'Age must be numeric and between 18 and 118');
-       }
-
-       // validate phone number
-        /*
-       if (validPhone($_POST['phone'])) {
-           $_SESSION['phone'] = $userPhone;
-       }
-       else {
-           $f3->set('errors["phone"]', 'Please enter a valid phone number');
-       }
-        */
-        $_SESSION['fName'] = $_POST['fName'];
-        $_SESSION['lName'] = $_POST['lName'];
-        $_SESSION['age'] = $_POST['age'];
-        $_SESSION['gender'] = $_POST['gender'];
-        $_SESSION['phone'] = $_POST['phone'];
-
-        // no errors
-        if (empty($f3->get('errors'))) {
-
-            // check if user is premium member
-            if(isset($_POST['premium'])){
-                $_SESSION['member'] = new PremiumMember($userFName, $userLName, $userAge, $userGender, $userPhone);
-            } else{
-                $_SESSION['member'] = new Member($userFName, $userLName, $userAge, $userGender, $userPhone);
-            }
-            $f3->reroute('/profile2');
-            //header('location: profile2');
-        }
-
-    }
-
-    //Get the data from the model
-    $f3->set('genders', getGender());
-
-    //store the user input to the hive
-    $f3->set('userFName', $userFName);
-    $f3->set('userLName', $userLName);
-    $f3->set('userAge', $userAge);
-    $f3->set('userPhone', $userPhone);
-    $f3->set('userGender', $userGender);
-
-    // Display the Personal info page
-    $view = new Template();
-    echo $view->render('views/personal-info.html');
+    $GLOBALS['con']->profile2();
 });
 
-// profile.html page
-$f3->route('GET|POST /profile2', function ($f3){
-    /* If the form has been submitted, add the data to session
-    * and send the user to the next order form
-    */
+//interest page
+$f3->route('GET|POST /profile3', function(){
 
-    //Initialize variables to store user input
-    $userEmail = "";
-
-    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-
-        //if email is valid store data
-        if (validEmail($userEmail)) {
-            $_SESSION['email'] = $userEmail;
-        }
-        //set an error if not valid
-        else {
-            $f3->set('errors["email"]', 'Please enter a valid email');
-        }
-
-        //var_dump($_POST);
-        $_SESSION['email'] = $_POST['email'];
-        $_SESSION['state'] = $_POST['state'];
-        $_SESSION['seeking'] = $_POST['seeking'];
-        $_SESSION['bio'] = $_POST['bio'];
-    }
-
-    // if no errors
-    if (empty($f3->get('errors'))) {
-        header('location: profile3');
-    }
-
-    //store the user input to the hive
-    $f3->set('userEmail', $userEmail);
-
-    // Display the Profile page
-    $view = new Template();
-    echo $view->render('views/profile.html');
-
+    $GLOBALS['con']->profile3();
 });
 
-// interest.html page
-$f3->route('GET|POST /profile3', function ($f3) {
+//summary
+$f3->route('GET|POST /summary', function(){
 
-    //Initialize variables for user input
-    $userIndoorChoices = array();
-    $userOutdoorChoices = array();
-
-    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-        if (!empty($_POST['indoor'])) {
-
-            //get user input
-            $userIndoorChoices = $_POST['indoor'];
-
-            //if indoor interests are valid
-            if (validIndoor($userIndoorChoices)) {
-                $_SESSION['indoor'] = implode(", ", $userIndoorChoices);
-            }
-            else {
-                $f3->set('errors["indoor"]', 'Invalid selection');
-            }
-        }
-
-        if (!empty($_POST['outdoor'])) {
-            //get user input
-            $userOutdoorChoices = $_POST['outdoor'];
-
-            //if indoor interests are valid
-            if (validOutdoor($userOutdoorChoices)) {
-                $_SESSION['outdoor'] = implode(", ", $userOutdoorChoices);
-            }
-            else {
-                $f3->set('errors["outdoor"]', 'Invalid selection');
-            }
-        }
-
-        if (empty($f3->get('errors'))) {
-            header('location: summary');
-        }
-    }
-
-    //Get the data from the model
-    $f3->set('indoorints', getIndoor());
-    $f3->set('outdoorints', getOutdoor());
-
-    //Store the user input in the hive
-    $f3->set('userIndoorChoices', $userIndoorChoices);
-    $f3->set('userOutdoorChoices', $userOutdoorChoices);
-
-    /* If the form has been submitted, add the data to session
-    * and send the user to the next order form
-
-    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-        //var_dump($_POST);
-        $_SESSION['profile2'] = implode(", ", $_POST['profile3  ']);
-
-        header('location: summary');
-    }
-    */
-
-    // Display the interest page
-    $view = new Template();
-    echo $view->render('views/interest.html');
-});
-
-// summary.html
-$f3->route('GET /summary', function () {
-    // Display the summary page
-    $view = new Template();
-    echo $view->render('views/summary.html');
+    $GLOBALS['con']->summary();
 });
 
 // run fat-free
